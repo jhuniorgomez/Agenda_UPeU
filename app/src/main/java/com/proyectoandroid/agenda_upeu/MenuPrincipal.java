d38191dc18cfefbfc6fe28b1a132f4e37a37a14b
junior <jhunior944@gmail.com>
2022-12-15 23:43:36 -0500

    first commit

---
+++ app/src/main/java/com/proyectoandroid/agenda_upeu/MenuPrincipal.java
@@ -0,0 +1,350 @@
+package com.proyectoandroid.agenda_upeu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.proyectoandroid.agenda_upeu.Notas.Agregar_Nota;
+import com.proyectoandroid.agenda_upeu.Notas.Listar_Notas;
+import com.proyectoandroid.agenda_upeu.Notas.Notas_Importantes;
+import com.proyectoandroid.agenda_upeu.Configuracion.Configuracion;
+import com.proyectoandroid.agenda_upeu.Contactos.Listar_Contactos;
+import com.proyectoandroid.agenda_upeu.Perfil.Perfil_Usuario;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    Button AgregarNotas, ListarNotas, Importantes,Contactos,AcercaDe,CerrarSesion;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    TextView UidPrincipal, NombresPrincipal, CorreoPrincipal;
+    Button EstadoCuentaPrincipal;
+    ProgressBar progressBarDatos;
+    ProgressDialog progressDialog;
+    LinearLayoutCompat Linear_Nombres , Linear_Correo ,Linear_Verificacion;
+
+    DatabaseReference Usuarios;
+
+    Dialog dialog_cuenta_verificada, dialog_informacion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+
+        UidPrincipal = findViewById(R.id.UidPrincipal);
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal = findViewById(R.id.CorreoPrincipal);
+        EstadoCuentaPrincipal = findViewById(R.id.EstadoCuentaPrincipal);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
+
+        dialog_cuenta_verificada = new Dialog(this);
+        dialog_informacion = new Dialog(this);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Espere por favor ...");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        Linear_Nombres = findViewById(R.id.Linear_Nombres);
+        Linear_Correo = findViewById(R.id.Linear_Correo);
+        Linear_Verificacion = findViewById(R.id.Linear_Verficacion);
+
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
+
+
+        AgregarNotas = findViewById(R.id.AgregarNotas);
+        ListarNotas = findViewById(R.id.ListarNotas);
+        Importantes = findViewById(R.id.Importantes);
+        Contactos = findViewById(R.id.Contactos);
+        AcercaDe = findViewById(R.id.AcercaDe);
+        CerrarSesion = findViewById(R.id.CerrarSesion);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        EstadoCuentaPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user.isEmailVerified()){
+                    //Si la cuenta está verificada
+                    //Toast.makeText(MenuPrincipal.this, "Cuenta ya verificada", Toast.LENGTH_SHORT).show();
+                    AnimacionCuentaVerificada();
+                }else {
+                    //Si la cuenta no está verificada
+                    VerificarCuentaCorreo();
+                }
+            }
+        });
+
+        AgregarNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*Obtenemos la información de los TextView*/
+                String uid_usuario = UidPrincipal.getText().toString();
+                String correo_usuario = CorreoPrincipal.getText().toString();
+
+                /*Pasamos datos a la siguiente actividad*/
+                Intent intent = new Intent(MenuPrincipal.this, Agregar_Nota.class);
+                intent.putExtra("Uid", uid_usuario);
+                intent.putExtra("Correo", correo_usuario);
+                startActivity(intent);
+
+            }
+        });
+
+        ListarNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuPrincipal.this, Listar_Notas.class));
+                Toast.makeText(MenuPrincipal.this, "Listar Notas", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Importantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuPrincipal.this, Notas_Importantes.class));
+                Toast.makeText(MenuPrincipal.this, "Notas Archivadas", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Contactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MenuPrincipal.this, "Contactos", Toast.LENGTH_SHORT).show();
+                /*Obteniendo el dato uid del usuario*/
+                String uid_usuario = UidPrincipal.getText().toString();
+                Intent intent = new Intent(MenuPrincipal.this, Listar_Contactos.class);
+                /*Enviamos el dato a la siguiente actividad*/
+                intent.putExtra("Uid", uid_usuario);
+                startActivity(intent);
+            }
+        });
+
+        AcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Informacion();
+            }
+        });
+
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalirAplicacion();
+            }
+        });
+    }
+
+    private void VerificarCuentaCorreo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("verificar cuenta")
+                .setMessage("¿Estás seguro(a) de enviar instrucciones de verificación a su correo electrónico? "
+                        +user.getEmail())
+                .setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EnviarCorreoAVerificacion();
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MenuPrincipal.this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+    }
+
+    private void EnviarCorreoAVerificacion() {
+        progressDialog.setMessage("Enviando instrucciones de verificación a su correo electrónico "+ user.getEmail());
+        progressDialog.show();
+
+        user.sendEmailVerification()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //Envío fue exitoso
+                        progressDialog.dismiss();
+                        Toast.makeText(MenuPrincipal.this, "Instrucciones enviadas, revise su bandeja " +user.getEmail(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Falló el envío
+                        Toast.makeText(MenuPrincipal.this, "Falló debido a: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void VerificarEstadoDeCuenta(){
+        String Verificado = "Verificado";
+        String No_Verificado = "No Verificado";
+        if (user.isEmailVerified()){
+            EstadoCuentaPrincipal.setText(Verificado);
+            EstadoCuentaPrincipal.setBackgroundColor(Color.rgb(41, 128, 185));
+        }else {
+            EstadoCuentaPrincipal.setText(No_Verificado);
+            EstadoCuentaPrincipal.setBackgroundColor(Color.rgb(231, 76, 60));
+        }
+    }
+
+    private void AnimacionCuentaVerificada(){
+        Button EntendidoVerificado;
+
+        dialog_cuenta_verificada.setContentView(R.layout.dialogo_cuenta_verificada);
+
+        EntendidoVerificado = dialog_cuenta_verificada.findViewById(R.id.EntendidoVerificado);
+
+        EntendidoVerificado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_cuenta_verificada.dismiss();
+            }
+        });
+
+        dialog_cuenta_verificada.show();
+        dialog_cuenta_verificada.setCanceledOnTouchOutside(false);
+    }
+
+    private void Informacion(){
+        Button EntendidoInfo;
+
+        dialog_informacion.setContentView(R.layout.cuadro_dialogo_informacion);
+
+        EntendidoInfo = dialog_informacion.findViewById(R.id.EntendidoInfo);
+
+        EntendidoInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog_informacion.dismiss();
+            }
+        });
+
+        dialog_informacion.show();
+        dialog_informacion.setCanceledOnTouchOutside(false);
+    }
+
+    @Override
+    protected void onStart() {
+        ComprobarInicioSesion();
+        super.onStart();
+    }
+
+    private void ComprobarInicioSesion(){
+        if (user!=null){
+            //El usuario ha iniciado sesión
+            CargaDeDatos();
+        }else {
+            //Lo dirigirá al MainActivity
+            startActivity(new Intent(MenuPrincipal.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CargaDeDatos(){
+
+        VerificarEstadoDeCuenta();
+
+        Usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //Si el usuario existe
+                if (snapshot.exists()){
+                    //El progressbar se oculta
+                    progressBarDatos.setVisibility(View.GONE);
+                    //Los TextView se muestran
+                    //UidPrincipal.setVisibility(View.VISIBLE);
+                    //NombresPrincipal.setVisibility(View.VISIBLE);
+                    //CorreoPrincipal.setVisibility(View.VISIBLE);
+                    Linear_Nombres.setVisibility(View.VISIBLE);
+                    Linear_Correo.setVisibility(View.VISIBLE);
+                    Linear_Verificacion.setVisibility(View.VISIBLE);
+
+                    //Obtener los datos
+                    String uid = ""+snapshot.child("uid").getValue();
+                    String nombres = ""+snapshot.child("nombres").getValue();
+                    String correo = ""+snapshot.child("correo").getValue();
+
+                    //Setear los datos en los respectivos TextView
+                    UidPrincipal.setText(uid);
+                    NombresPrincipal.setText(nombres);
+                    CorreoPrincipal.setText(correo);
+
+                    //Habilitar los botones del menú
+                    AgregarNotas.setEnabled(true);
+                    ListarNotas.setEnabled(true);
+                    Importantes.setEnabled(true);
+                    Contactos.setEnabled(true);
+                    AcercaDe.setEnabled(true);
+                    CerrarSesion.setEnabled(true);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.Perfil_usuario){
+            startActivity(new Intent(MenuPrincipal.this, Perfil_Usuario.class));
+        }
+        if (item.getItemId() == R.id.Configuracion){
+            String uid_usuario = UidPrincipal.getText().toString();
+
+            Intent intent = new Intent(MenuPrincipal.this, Configuracion.class);
+            intent.putExtra("Uid", uid_usuario);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SalirAplicacion() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/proyectoandroid/agenda_upeu/Notas/Agregar_Nota.java
@@ -0,0 +1,199 @@
+package com.proyectoandroid.agenda_upeu.Notas;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.proyectoandroid.agenda_upeu.Objetos.Nota;
+import com.proyectoandroid.agenda_upeu.R;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Agregar_Nota extends AppCompatActivity {
+
+    TextView Uid_Usuario, Correo_usuario, Fecha_hora_actual, Fecha, Estado;
+    EditText Titulo, Descripcion;
+    Button Btn_Calendario;
+
+    int dia, mes , anio;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    DatabaseReference BD_Firebase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_nota);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        InicializarVariables();
+        ObtenerDatos();
+        Obtener_Fecha_Hora_Actual();
+
+        Btn_Calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendario = Calendar.getInstance();
+
+                dia = calendario.get(Calendar.DAY_OF_MONTH);
+                mes = calendario.get(Calendar.MONTH);
+                anio = calendario.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Agregar_Nota.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int AnioSeleccionado, int MesSeleccionado, int DiaSeleccionado) {
+
+                        String diaFormateado, mesFormateado;
+
+                        //OBTENER DIA
+                        if (DiaSeleccionado < 10){
+                            diaFormateado = "0"+String.valueOf(DiaSeleccionado);
+                            // Antes: 9/11/2022 -  Ahora 09/11/2022
+                        }else {
+                            diaFormateado = String.valueOf(DiaSeleccionado);
+                            //Ejemplo 13/08/2022
+                        }
+
+                        //OBTENER EL MES
+                        int Mes = MesSeleccionado + 1;
+
+                        if (Mes < 10){
+                            mesFormateado = "0"+String.valueOf(Mes);
+                            // Antes: 09/8/2022 -  Ahora 09/08/2022
+                        }else {
+                            mesFormateado = String.valueOf(Mes);
+                            //Ejemplo 13/10/2022 - 13/11/2022 - 13/12/2022
+
+                        }
+
+                        //Setear fecha en TextView
+                        Fecha.setText(diaFormateado + "/" + mesFormateado + "/"+ AnioSeleccionado);
+
+                    }
+                }
+                        ,anio,mes,dia);
+                datePickerDialog.show();
+
+            }
+        });
+    }
+
+    private void InicializarVariables(){
+        Uid_Usuario = findViewById(R.id.Uid_Usuario);
+        Correo_usuario = findViewById(R.id.Correo_usuario);
+        Fecha_hora_actual = findViewById(R.id.Fecha_hora_actual);
+        Fecha = findViewById(R.id.Fecha);
+        Estado = findViewById(R.id.Estado);
+
+        Titulo = findViewById(R.id.Titulo);
+        Descripcion = findViewById(R.id.Descripcion);
+        Btn_Calendario = findViewById(R.id.Btn_Calendario);
+
+        BD_Firebase = FirebaseDatabase.getInstance().getReference("Usuarios");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+    }
+
+    private void ObtenerDatos(){
+        String uid_recuperado = getIntent().getStringExtra("Uid");
+        String correo_recuperado = getIntent().getStringExtra("Correo");
+
+        Uid_Usuario.setText(uid_recuperado);
+        Correo_usuario.setText(correo_recuperado);
+    }
+
+    private void Obtener_Fecha_Hora_Actual(){
+        String Fecha_hora_registro = new SimpleDateFormat("dd-MM-yyyy/HH:mm:ss a",
+                Locale.getDefault()).format(System.currentTimeMillis());
+        //EJEMPLO: 13-11-2022/06:30:20 pm
+        Fecha_hora_actual.setText(Fecha_hora_registro);
+    }
+
+    private void Agregar_Nota(){
+
+        //Obtener los datos
+        String uid_usuario = Uid_Usuario.getText().toString();
+        String correo_usuario = Correo_usuario.getText().toString();
+        String fecha_hora_actual = Fecha_hora_actual.getText().toString();
+        String titulo = Titulo.getText().toString();
+        String descripcion = Descripcion.getText().toString();
+        String fecha = Fecha.getText().toString();
+        String estado = Estado.getText().toString();
+        String id_nota = BD_Firebase.push().getKey();
+
+        //Validar datos
+        if (!uid_usuario.equals("") && !correo_usuario.equals("") && !fecha_hora_actual.equals("") &&
+                !titulo.equals("") && !descripcion.equals("") && ! fecha.equals("") && !estado.equals("")){
+
+            Nota nota = new Nota(id_nota,
+                    uid_usuario,
+                    correo_usuario,
+                    fecha_hora_actual,
+                    titulo,
+                    descripcion,
+                    fecha,
+                    estado);
+
+
+            //Establecer el nombre de la BD
+            String Nombre_BD = "Notas_Publicadas";
+
+            assert id_nota != null;
+            BD_Firebase.child(user.getUid()).child(Nombre_BD).child(id_nota).setValue(nota);
+
+            Toast.makeText(this, "Se ha agregado la nota exitosamente", Toast.LENGTH_SHORT).show();
+            onBackPressed();
+
+        }
+        else {
+            Toast.makeText(this, "Llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_agregar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.Agregar_Nota_BD) {
+            Agregar_Nota();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
